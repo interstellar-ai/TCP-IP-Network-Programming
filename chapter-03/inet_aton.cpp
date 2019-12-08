8c56468e371c15afe0e43c2df69b659c61dda0c3
interstellar-ai <qianli.chen.ai@foxmail.com>
2019-12-08 21:27:00 +0800

    a to n and n to a

--- chapter-03/inet_aton.cpp
+++ chapter-03/inet_aton.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <arpa/inet.h>
+#include <string.h>
+#include <stdlib.h>
 
 using namespace std;
+
+void error_handle(string error_log) {
+	cout << error_log << endl;
+	exit(-1);
+}
 
 int main(int argc, char const *argv[])
 {
@@ -15,5 +22,14 @@ int main(int argc, char const *argv[])
 	else
 		cout << "conversion failed ..." << endl;
 
+	char *conv_addr = inet_ntoa(addr_inet.sin_addr);
+	if (*conv_addr == -1) {
+		error_handle("conversion error");
+	}
+	char str_ip[20];
+	// inet_ntoa()函数返回的是一个字符串指针，这个指针指向一个inet_ntoa函数申请的内存，
+	// 应该在下次调用此函数前将字符串的内容复制到其他地方
+	strcpy(str_ip, conv_addr);
+	cout << "conversion str_ip: " << str_ip << endl;
 	return 0;
 }
