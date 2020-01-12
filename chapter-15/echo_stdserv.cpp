3d827b5b507b60930d7840e9d56bf0dc11c811f7
interstellar-ai <qianli.chen.ai@foxmail.com>
2020-01-12 16:33:17 +0800

    std io cmu

---
+++ chapter-15/echo_stdserv.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include "stdio.h"
+
+using namespace std;
+
+#ifndef bufsize
+#define bufsize 1024
+#endif
+
+void error_handle( string str ) {
+	cout << str << endl;
+	exit(1);
+}
+
+int main(int argc, char* argv[]){
+	int serv_sock, clnt_sock;
+
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in clnt_addr;
+
+	char message[bufsize];
+
+	FILE *readfp;
+	FILE *writefp;
+
+	if (argc != 2) {
+		cout << "Usage : " << argv[0] << " <port> " << endl;
+		return -1;
+	}
+
+	serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (serv_sock == -1){
+		cout << "Create server socket failed ..." << endl;
+		return -1;
+	}
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(atoi(argv[1]));
+
+	int ret = bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+	if (ret == -1) {
+		cout << "bind failed ..." << endl;
+		return -1;
+	}
+
+	ret = listen(serv_sock, 5);
+	if (ret == -1) {
+		cout << "listen failed ..." << endl;
+		return -1;
+	}
+
+	socklen_t clnt_addr_size = sizeof(clnt_addr);
+	char serv_flag[] = " from server";
+	for(int i = 0; i < 5; i++) {
+		clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+		if (clnt_sock == -1) {
+			cout << "accept failed ..." << endl;
+			return -1;
+		}
+		readfp = fdopen(clnt_sock, "r");
+		writefp = fdopen(clnt_sock, "w");
+		while(!feof(readfp)) {
+			fgets(message, sizeof(message), readfp);
+			cout << message << endl;
+			fputs(message, writefp);
+			fflush(writefp);
+		}
+		fclose(readfp);
+		fclose(writefp);
+	}
+	close(serv_sock);
+	return 0;
+}
